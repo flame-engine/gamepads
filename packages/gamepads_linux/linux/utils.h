7812e56bccc18ef3a4d0fba12ca2ee919ecc1795
Luan Nico <luanpotter27@gmail.com>
2023-03-25 21:41:11 -0400

    Remove unused string_format

--- packages/gamepads_linux/linux/utils.h
+++ packages/gamepads_linux/linux/utils.h
@@ -2,8 +2,4 @@
 #include <string>
 #include <stdexcept>
 
-// TODO(luan): temporary polyfill for std format bc flutter doesn't seem to support it
-// will remove once we have a data structure for events anyway
-const std::string string_format(const char * const zcFormat, ...);
-
 bool starts_with(const std::string& string, const std::string& prefix);
