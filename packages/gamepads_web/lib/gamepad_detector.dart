f1ee9e5f95f651cf8a9c4e89393c5a6cc063f6b9
zhuhaichao518 <zhuhaichao518@gmail.com>
2024-10-24 13:39:00 +0800

    use js_interop and package:web instead of package:js and dart:html

--- packages/gamepads_web/lib/gamepad_detector.dart
+++ packages/gamepads_web/lib/gamepad_detector.dart
@@ -1,29 +1,65 @@
-// gamepad_detector.dart
-// ignore_for_file: omit_local_variable_types, avoid_dynamic_calls
+import 'dart:js_interop';
 
 import 'package:gamepads_platform_interface/api/gamepad_controller.dart';
 import 'package:gamepads_platform_interface/gamepads_platform_interface.dart';
-import 'package:js/js.dart';
-import 'package:js/js_util.dart';
 
-@JS('navigator.getGamepads')
-external dynamic _getGamepads();
+@JS('navigator')
+external Navigator get navigator;
+
+@JS()
+@staticInterop
+class Navigator {}
+
+@JS()
+@staticInterop
+class JSArray {}
+
+@JS()
+@staticInterop
+class Gamepad {}
+
+extension NavigatorGamepads on Navigator {
+  external JSArray getGamepads();
+}
+
+// Extension for JSArray to handle array access
+extension JSArrayInterop on JSArray {
+  external int get length;
+  external JSAny? operator [](int index);
+}
+
+// Extension for Gamepad to access its properties
+extension GamepadInterop on Gamepad {
+  external int get index;
+  external String get id;
+}
 
 List<GamepadController> getGamepads(GamepadsPlatformInterface plugin) {
   final controllers = <GamepadController>[];
-  final gamepads = _getGamepads();
-  for (int i = 0; i < gamepads.length; i++) {
-    final gamepad = getProperty(gamepads, i.toString());
+  final gamepads = navigator.getGamepads();
+
+  for (var i = 0; i < gamepads.length; i++) {
+    final gamepad = gamepads[i] as Gamepad?;
     if (gamepad != null) {
       controllers.add(
-          GamepadController(id: gamepad.index.toString(),
-                            name: gamepad.id,
-                            plugin: plugin,),);
+        GamepadController(
+          id: gamepad.index.toString(),
+          name: gamepad.id,
+          plugin: plugin,
+        ),
+      );
     }
   }
   return controllers;
 }
 
 List<dynamic> getGamepadList() {
-  return _getGamepads();
+  final gamepads = navigator.getGamepads();
+  return List.generate(gamepads.length, (i) {
+    final gamepad = gamepads[i];
+    if (gamepad != null) {
+      return gamepad;
+    }
+    return null;
+  }).where((gamepad) => gamepad != null).toList();
 }
--- packages/gamepads_web/lib/gamepads_web.dart
+++ packages/gamepads_web/lib/gamepads_web.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: avoid_dynamic_calls, omit_local_variable_types
 
 import 'dart:async';
-import 'dart:html' as html show window;
-import 'dart:js_util';
+import 'dart:js_interop';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -11,13 +10,14 @@ import 'package:gamepads_platform_interface/api/gamepad_event.dart';
 import 'package:gamepads_platform_interface/gamepads_platform_interface.dart';
 
 import 'package:gamepads_web/gamepad_detector.dart';
+import 'package:web/web.dart' as web;
 
 class GamePadState {
   GamePadState(int length) {
-    keyStates = List<dynamic>.filled(length, null, growable: true);
-    axesStates = List<dynamic>.filled(4, null, growable: true);
+    keyStates = List<dynamic>.filled(length, null);
+    axesStates = List<dynamic>.filled(4, null);
   }
-  
+
   List<dynamic>? keyStates;
   List<dynamic>? axesStates;
 }
@@ -26,47 +26,51 @@ class GamePadState {
 class GamepadsWeb extends GamepadsPlatformInterface {
   int _gamepadCount = 0;
   Timer? _gamepadPollingTimer;
-  
-  Map<String,GamePadState> lastGamePadstates = {};
-  
+
+  Map<String, GamePadState> lastGamePadstates = {};
+
   void updateGamepadsStatus() {
     final gamepads = getGamepadList();
     for (int i = 0; i < gamepads.length; i++) {
-      final gamepad = getProperty(gamepads, i.toString());
+      final gamepad = gamepads[i];
       if (gamepad != null) {
         final int buttoncount = gamepad.buttons.length;
         final String gamepadId = gamepad.index.toString();
         GamePadState lastState;
-        if (lastGamePadstates.containsKey(gamepadId) && 
-            lastGamePadstates[gamepadId]?.keyStates?.length == buttoncount){
+        if (lastGamePadstates.containsKey(gamepadId) &&
+            lastGamePadstates[gamepadId]?.keyStates?.length == buttoncount) {
           lastState = lastGamePadstates[gamepadId]!;
         } else {
           lastGamePadstates[gamepadId] = GamePadState(buttoncount);
           lastState = lastGamePadstates[gamepadId]!;
         }
         for (int i = 0; i < buttoncount; i++) {
-          if (lastState.keyStates?[i] != gamepad.buttons[i].value){
+          if (lastState.keyStates?[i] != gamepad.buttons[i].value) {
             lastState.keyStates?[i] = gamepad.buttons[i].value;
-            emitGamepadEvent(GamepadEvent(
-              gamepadId: gamepadId,
-              timestamp: DateTime.now().millisecondsSinceEpoch,
-              type: KeyType.button,
-              key: 'button $i',
-              value: gamepad.buttons[i].value,
-            ),);
+            emitGamepadEvent(
+              GamepadEvent(
+                gamepadId: gamepadId,
+                timestamp: DateTime.now().millisecondsSinceEpoch,
+                type: KeyType.button,
+                key: 'button $i',
+                value: gamepad.buttons[i].value,
+              ),
+            );
           }
         }
         for (int i = 0; i < 4; i++) {
-          if (lastState.keyStates?[i] != gamepad.axes[i]){
-            if (gamepad.axes[i]> 0.1 || gamepad.axes[i]< -0.1){
+          if (lastState.keyStates?[i] != gamepad.axes[i]) {
+            if (gamepad.axes[i] > 0.1 || gamepad.axes[i] < -0.1) {
               lastState.axesStates?[i] = gamepad.axes[i];
-              emitGamepadEvent(GamepadEvent(
-                gamepadId: gamepadId,
-                timestamp: DateTime.now().millisecondsSinceEpoch,
-                type: KeyType.analog,
-                key: 'analog $i',
-                value: gamepad.axes[i],
-              ),);
+              emitGamepadEvent(
+                GamepadEvent(
+                  gamepadId: gamepadId,
+                  timestamp: DateTime.now().millisecondsSinceEpoch,
+                  type: KeyType.analog,
+                  key: 'analog $i',
+                  value: gamepad.axes[i],
+                ),
+              );
             }
           }
         }
@@ -76,31 +80,37 @@ class GamepadsWeb extends GamepadsPlatformInterface {
 
   /// Constructs a GamepadsWeb
   GamepadsWeb() {
-    html.window.addEventListener('gamepadconnected', (event) {
-      _gamepadCount++;
-      if (_gamepadCount == 1){
-        // The game pad state for web is not event driven. We need to
-        // query the game pad state by ourself.
-        // By default we set the query interval is 1ms. 
-        _gamepadPollingTimer = Timer.periodic(
-        const Duration(milliseconds: 1), (timer) {
-          updateGamepadsStatus();
-        });
-      }
-    });
+    web.window.addEventListener(
+      'gamepadconnected',
+      (web.Event event) {
+        _gamepadCount++;
+        if (_gamepadCount == 1) {
+          // The game pad state for web is not event driven. We need to
+          // query the game pad state by ourself.
+          // By default we set the query interval is 1ms.
+          _gamepadPollingTimer =
+              Timer.periodic(const Duration(milliseconds: 1), (timer) {
+            updateGamepadsStatus();
+          });
+        }
+      }.toJS,
+    );
 
-    html.window.addEventListener('gamepaddisconnected', (event) {
-      _gamepadCount--;
-      if (_gamepadCount == 0){
-        _gamepadPollingTimer?.cancel();
-      }
-    });
+    web.window.addEventListener(
+      'gamepaddisconnected',
+      (web.Event event) {
+        _gamepadCount--;
+        if (_gamepadCount == 0) {
+          _gamepadPollingTimer?.cancel();
+        }
+      }.toJS,
+    );
   }
 
   static void registerWith(Registrar registrar) {
     GamepadsPlatformInterface.instance = GamepadsWeb();
   }
-  
+
   List<GamepadController>? controllers;
 
   @override
