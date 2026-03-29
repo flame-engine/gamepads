d20a7189cef66d8c1e4851375e1bdcf413bd80b8
Lisette Linse <lisette.linse@gmail.com>
2026-03-29 17:01:13 +0200

    test: Add test for GamepadInterceptor

--- packages/flutter_gamepads/test/flutter_gamepads_test.dart
+++ packages/flutter_gamepads/test/flutter_gamepads_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gamepads/src/gamepad_control.dart';
+import 'package:flutter_gamepads/src/gamepad_interceptor.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:gamepads/gamepads.dart';
@@ -15,10 +16,11 @@ final platformInterface =
 enum _UiButton { noButton, first, second }
 
 void main() {
+  Gamepads.normalizer = GamepadNormalizer.forPlatform(
+    GamepadPlatform.windows,
+  );
+
   testWidgets('GamepadControl', (WidgetTester tester) async {
-    Gamepads.normalizer = GamepadNormalizer.forPlatform(
-      GamepadPlatform.windows,
-    );
     var lastButtonPressed = _UiButton.noButton;
     var beforeInvokeActivate = false;
     var beforeInvokeDismiss = false;
@@ -127,6 +129,78 @@ void main() {
     expect(lastButtonPressed, equals(_UiButton.noButton));
     expect(beforeInvokeActivate, isFalse);
     expect(beforeInvokeDismiss, isFalse);
+  });
+
+  testWidgets('GamepadInterceptor', (WidgetTester tester) async {
+    var rootBeforeIntentCalled = false;
+    var rootEmit = true;
+    var interceptorBeforeIntentCalled = false;
+    var interceptorEmit = true;
+    final secondFocusNode = FocusNode();
+    var secondPressed = false;
+    final widget = MaterialApp(
+      home: GamepadControl(
+        onBeforeIntent: (intent) {
+          rootBeforeIntentCalled = true;
+          return rootEmit;
+        },
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => {},
+              child: const Text('Button'),
+            ),
+            GamepadInterceptor(onBeforeIntent: (intent) {
+              interceptorBeforeIntentCalled = true;
+              return interceptorEmit;
+            }, child: ElevatedButton(
+              focusNode: secondFocusNode,
+              onPressed: () { secondPressed = true; },
+              child: const Text('Second'),
+            ))
+          ],
+        ),
+      ),
+    );
+
+    await tester.pumpWidget(widget);
+    await _event('dpadDown');
+    await _event('dpadDown');
+    await tester.pumpAndSettle();
+
+    expect(secondFocusNode.hasFocus, isTrue);
+    expect(secondPressed, isFalse);
+
+    // Emit gamepad event => should call both onBeforeIntent
+    rootBeforeIntentCalled = false;
+    interceptorBeforeIntentCalled = false;
+    await _event('a');
+    await tester.pumpAndSettle();
+    expect(rootBeforeIntentCalled, isTrue);
+    expect(interceptorBeforeIntentCalled, isTrue);
+    expect(secondPressed, isTrue);
+
+    // Return false only from the root onBeforeIntent
+    rootEmit = false;
+    rootBeforeIntentCalled = false;
+    interceptorBeforeIntentCalled = false;
+    secondPressed = false;
+    await _event('a');
+    await tester.pumpAndSettle();
+    expect(rootBeforeIntentCalled, isTrue);
+    expect(interceptorBeforeIntentCalled, isTrue);
+    expect(secondPressed, isFalse);
+
+    // Return false only from the interceptor onBeforeIntent
+    rootEmit = true;
+    interceptorEmit = false;
+    rootBeforeIntentCalled = false;
+    interceptorBeforeIntentCalled = false;
+    await _event('a');
+    await tester.pumpAndSettle();
+    expect(rootBeforeIntentCalled, isFalse);
+    expect(interceptorBeforeIntentCalled, isTrue);
+    expect(secondPressed, isFalse);
   });
 }
 
