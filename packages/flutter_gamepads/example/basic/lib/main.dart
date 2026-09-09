b634ce644bd942838243e3d0d97d4a30bfb1d734
Lisette Linse <lisette.linse@gmail.com>
2026-09-09 10:43:45 +0200

    docs: Update basic example to use same example as README.md

--- packages/flutter_gamepads/example/basic/lib/main.dart
+++ packages/flutter_gamepads/example/basic/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
+
 import 'package:flutter_gamepads/flutter_gamepads.dart';
+import 'package:material_ui/material_ui.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,39 +12,75 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // GamepadControl enables using the app with a gamepad.
-    return const GamepadControl(child: MaterialApp(home: HomeScreen()));
+    // GamepadControl enables controlling the app with a gamepad.
+    //
+    // In many cases this is all you need. So start with just this
+    // and test your app to see if there are cases that also need
+    // to use a GamepadInterceptor to workaround issues.
+    return const GamepadControl(
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool switchValue = false;
+  double sliderValue = 0.5;
+  double slider2Value = 0.5;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ElevatedButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Hello'),
-              ),
-            );
-          },
-          child: const Text('Button 1'),
+        SwitchListTile(
+          title: const Text('Works with gamepads'),
+          value: switchValue,
+          onChanged: (value) => setState(() => switchValue = value),
         ),
         ElevatedButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('World'),
-              ),
-            );
-          },
-          child: const Text('Button 2'),
+          onPressed: () {},
+          child: const Text('Can be clicked with gamepad'),
         ),
-        const ElevatedButton(onPressed: null, child: Text('Disabled button')),
+        // This can be focused, but gamepad users cannot change the value
+        // The solution is given below.
+        Slider(
+          value: sliderValue,
+          label: 'Does not work with gamepads',
+          onChanged: (value) => setState(() => sliderValue = value),
+        ),
+        // This slider can be operated with Gamepad due to the
+        // compatibility layer provided via GamepadInterceptor.
+        GamepadInterceptor(
+          onBeforeIntent: (activator, intent) {
+            if (intent is ScrollIntent) {
+              if (intent.direction == AxisDirection.right) {
+                setState(() => slider2Value = min(1.0, slider2Value + 0.1));
+              } else if (intent.direction == AxisDirection.left) {
+                setState(() => slider2Value = max(0.0, slider2Value - 0.1));
+              }
+              // Block actual emit of ScrollIntent
+              return false;
+            }
+            // Allow other intents such as focus change to occur
+            return true;
+          },
+          child: Slider(
+            value: slider2Value,
+            label: 'Works with gamepads',
+            // This setState never occur by Gamepad input, but is
+            // good to allow keyboard/mouse input as well.
+            onChanged: (value) => setState(() => slider2Value = value),
+          ),
+        ),
       ],
     );
   }
