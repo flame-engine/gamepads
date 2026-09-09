f5f8f08f50d3152ac6042d9d772649df05e4e678
Lisette Linse <lisette.linse@gmail.com>
2026-09-09 11:09:33 +0200

    revert: keep min SDK of basic example same as root min SDK

--- packages/flutter_gamepads/example/basic/lib/main.dart
+++ packages/flutter_gamepads/example/basic/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_gamepads/flutter_gamepads.dart';
-import 'package:material_ui/material_ui.dart';
 
 void main() {
   runApp(const MyApp());
